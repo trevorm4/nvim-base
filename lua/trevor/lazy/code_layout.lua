9948ea47d6f166043908c73721085c2560c6a904
Trevor Moyer <trevormoyer@google.com>
2026-03-02 18:29:24 +0000

    Update code_layout

--- lua/trevor/lazy/code_layout.lua
+++ lua/trevor/lazy/code_layout.lua
@@ -1,20 +1,25 @@
 return {
-  {
-    "trevorm4/code-layout.nvim",
-    keys = {
-      {
-        "<leader>o",
-        "<cmd>CodeLayoutRight<cr>",
-        desc = "Open LSP symbol tree",
-      },
-      {
-        "<leader>lf",
-        "<cmd>CodeLayoutFloat<cr>",
-        desc = "Open floating layout window",
-      },
-    },
-    config = function()
-      -- Plugin logic is handled via the layout() API
-    end,
-  },
+	{
+		"trevorm4/code-layout.nvim",
+		branch = "dev",
+		keys = {
+			{
+				"<leader>o",
+				"<cmd>CodeLayoutRight<cr>",
+				desc = "Open LSP symbol tree",
+			},
+			{
+				"<leader>lf",
+				"<cmd>CodeLayoutFloat<cr>",
+				desc = "Open floating layout window",
+			},
+		},
+		config = function()
+			require("code_layout").setup({
+				floating = {
+					annotate = true, -- Set to false to hide the full symbol path in the fzf window
+				},
+			})
+		end,
+	},
 }
