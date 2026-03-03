32e402fc3824581fbda77a14ed178d2e761f4d83
Trevor Moyer <trevormoyer@google.com>
2026-03-03 23:06:25 +0000

    Change to jjui

---
+++ lazy/float.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      local toggleterm = require("toggleterm")
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      toggleterm.setup({
+        open_mapping = nil,
+        direction = "float",
+        float_opts = {
+          border = "curved",
+          winblend = 3,
+        },
+      })
+
+      ---@param name string Name for the keymap description
+      ---@param command string The binary to run
+      ---@param keymap string The key sequence to trigger it
+      local function register_floating_binary(name, command, keymap)
+        local term = Terminal:new({
+          cmd = command,
+          hidden = true,
+          on_open = function(t)
+            vim.cmd("startinsert!")
+            -- Allow toggling it off with the same keybind while inside the terminal
+            vim.api.nvim_buf_set_keymap(t.bufnr, "t", keymap, "<cmd>close<CR>", { silent = true })
+          end,
+        })
+
+        vim.keymap.set("n", keymap, function() term:toggle() end, {
+          noremap = true,
+          silent = true,
+          desc = "Toggle " .. name,
+        })
+      end
+
+      register_floating_binary("JJUI", "jjui", "<leader>jj")
+    end,
+  },
+}
+
